a9951de938efa95fddb86ebee47d9e9b1e9626dc
James Lockhart <james.lockhart117@gmail.com>
2018-02-27 14:05:39 +0300

    Updated classes and finish program

--- GoFish/GoFish/Card.cs
+++ GoFish/GoFish/Card.cs
@@ -8,63 +8,58 @@ namespace GoFish
 
 {
 
-    public enum Suit
-    {
-       Spades,
-       Clubs,
-       Diamonds,
-       Hearts
-    }
-
-    public enum Value
-    {
-       Ace = 1,
-       Two = 2,
-       Three = 3,
-       Four = 4,
-       Five = 5,
-       Six = 6,
-       Seven = 7,
-       Eight = 8,
-       Nine = 9,
-       Ten = 10,
-       Jack = 11,
-       Queen = 12,
-       King = 13
-    }
-
 
     public partial class Card
     {
-        public Suit Suit { get; set; }
-        public Value Value { get; set; }
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
 
-        public Card(Suit suits, Value values)
+        public Card(Suits suit, Values value)
         {
-            Suit = suits;
-            Value = values;
+            this.Suit = suit;
+            this.Value = value;
         }
 
         public string Name
         {
-            get
+            get { return Value.ToString() + " of " + Suit.ToString(); }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Suits suit)
+        {
+            if (cardToCheck.Suit == suit)
             {
-                return Value.ToString() + " of " + Suit.ToString();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Values value)
+        {
+            if (cardToCheck.Value == value)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
         public static string Plural(Values value)
         {
             if (value == Values.Six)
-            {
                 return "Sixes";
-            }
             else
-            {
                 return value.ToString() + "s";
-            }
         }
-        
-
     }
 }
--- GoFish/GoFish/Deck.cs
+++ GoFish/GoFish/Deck.cs
@@ -10,24 +10,17 @@ namespace GoFish
     public class Deck
     {
 
-        //deck stores its cards in a list-but uses encapsulation
         private List<Card> cards;
         private Random random = new Random();
 
-        //adding parameters to keep it from making a complete deck
         public Deck()
         {
             cards = new List<Card>();
             for (int suit = 0; suit <= 3; suit++)
-            {
                 for (int value = 1; value <= 13; value++)
-                {
-                    cards.Add(new Card((Suit)suit, (Value)value));
-                }
-            }
+                    cards.Add(new Card((Suits)suit, (Values)value));
         }
 
-        //overload constructor to make the inital deck
         public Deck(IEnumerable<Card> initialCards)
         {
             cards = new List<Card>(initialCards);
@@ -40,7 +33,6 @@ namespace GoFish
             cards.Add(cardToAdd);
         }
 
-        //deal a card out of the deck, remove the card object and return a reference to it.
         public Card Deal(int index)
         {
             Card CardToDeal = cards[index];
@@ -48,26 +40,23 @@ namespace GoFish
             return CardToDeal;
         }
 
-        //shuffle cards
         public void Shuffle()
         {
-            List<Card> newCards = new List<Card>();
+            List<Card> NewCards = new List<Card>();
             while (cards.Count > 0)
             {
                 int CardToMove = random.Next(cards.Count);
-                newCards.Add(cards[CardToMove]);
+                NewCards.Add(cards[CardToMove]);
                 cards.RemoveAt(CardToMove);
             }
+            cards = NewCards;
         }
 
-        //return a string array that contains each card's name
         public IEnumerable<string> GetCardNames()
         {
             string[] CardNames = new string[cards.Count];
             for (int i = 0; i < cards.Count; i++)
-            {
                 CardNames[i] = cards[i].Name;
-            }
             return CardNames;
         }
 
@@ -76,70 +65,48 @@ namespace GoFish
             cards.Sort(new CardComparer_bySuit());
         }
 
-        //look at a card in the deck without deleting it
         public Card Peek(int cardNumber)
         {
             return cards[cardNumber];
         }
 
-        //if no parameters are pass, deal a card off the top of the deck
         public Card Deal()
         {
             return Deal(0);
         }
 
-        //search through deck for card with certain value
         public bool ContainsValue(Values value)
         {
             foreach (Card card in cards)
-            {
                 if (card.Value == value)
-                {
                     return true;
-                }
-                return false;
-            }
+            return false;
         }
 
-        //build the book of cards from deck
         public Deck PullOutValues(Values value)
         {
             Deck deckToReturn = new Deck(new Card[] { });
             for (int i = cards.Count - 1; i >= 0; i--)
-            {
                 if (cards[i].Value == value)
-                {
                     deckToReturn.Add(Deal(i));
-                }
-            }
             return deckToReturn;
         }
 
-        //check to see if a book contains 4 cards
         public bool HasBook(Values value)
         {
             int NumberOfCards = 0;
             foreach (Card card in cards)
-            {
                 if (card.Value == value)
-                {
                     NumberOfCards++;
-                }
-            }
             if (NumberOfCards == 4)
-            {
                 return true;
-            }
             else
-            {
                 return false;
-            }
         }
-    
-        //sort deck
+
         public void SortByValue()
         {
-            cards.Sort(new CardComparer_bySuit());
+            //cards.Sort(new CardComparer_byValue());
         }
     }
 }
